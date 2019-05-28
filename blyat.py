96b89313736eb54a0d8da9ee57d656dbbc2e558d
Dennis Israelsson <dennis.israelsson@gmail.com>
2019-05-28 19:54:31 +0200

    add date support

--- blyat.py
+++ blyat.py
@@ -3,11 +3,16 @@
 
 # from mysql.connector import connect, errors
 from sqlite3 import connect, IntegrityError, OperationalError
+from re import search
 COMMANDS = ['!help', '!user add', '!beer add', '!beer remove', '!score']
 
 
 class UserExists(Exception):
     ''' Custom exception for when user already exists '''
+
+
+class InvalidDate(Exception):
+    ''' Custom exception for when date input is invalid '''
 
 
 class Blyat():
@@ -35,13 +40,14 @@ class Blyat():
                              'score INTEGER DEFAULT 0, '
                              'last_updated TIMESTAMP '
                              'DEFAULT CURRENT_TIMESTAMP);')
+        self._cursor.execute('CREATE TABLE beer_dates ('
+                             'date date PRIMARY KEY);')
 
     def user_add(self, user):
         ''' Add a user to the scoreboard '''
         try:
-            add_user = ('INSERT INTO scoreboard (username) '
-                        'VALUES (?)')
-            self._cursor.execute(add_user, (user,))
+            user_add = 'INSERT INTO scoreboard (username) VALUES (?)'
+            self._cursor.execute(user_add, (user,))
         except IntegrityError:
             raise UserExists()
 
@@ -54,12 +60,18 @@ class Blyat():
 
     def show_score(self):
         ''' Return the scoreboard '''
-        self._cursor.execute('SELECT username, score, '
-                             'DATE(last_updated) as date '
-                             'FROM scoreboard '
-                             'ORDER BY score DESC;')
+        self._cursor.execute('SELECT username, score, DATE(last_updated) AS '
+                             'date FROM scoreboard ORDER BY score DESC;')
 
         return self._cursor.fetchall()
 
     def date_add(self, date):
         ''' Add potential beer dates '''
+        if not search('^\\d{4}-\\d{2}-\\d{2}$', date):
+            raise InvalidDate()
+
+        try:
+            date_add = 'INSERT INTO beer_dates (date) VALUES (?)'
+            self._cursor.execute(date_add, (date,))
+        except IntegrityError:
+            raise UserExists()
